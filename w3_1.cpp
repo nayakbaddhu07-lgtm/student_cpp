97f0894375bb44c350c6b77ad0ae61ec4139b5fb
nayakbaddhu07-lgtm <nayakbaddhu07@gmail.com>
2026-02-21 13:46:55 +0530

    Add files via upload

---
+++ w3_1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+    vector<int> v;
+    int n;
+    cout<<"enter no of elements:"<<endl;
+    cin>>n;
+    for(int i=0; i<n; i++) {
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    cout<<"vector elements are:"<<endl;
+    for(vector<int>::iterator it=v.begin(); it!=v.end(); it++) {
+        cout<<*it<<" ";
+    }  return 1;
+} 
